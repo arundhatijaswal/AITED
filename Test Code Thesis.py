b7114f91417626f598abf18448d4bd0c74592d11
divir94 <divir94@gmail.com>
2014-04-14 17:59:47 -0500

    filter search

--- Test Code Thesis.py
+++ Test Code Thesis.py
@@ -1,11 +1,22 @@
 import urllib
 import json as m_json
-query = raw_input ( 'Query: ' )
-query = urllib.urlencode ( { 'q' : query } )
+
+keyword = 'sports'
+searchQuery = 'issues in ' + keyword + ' site:businessinsider.com'
+
+query = urllib.urlencode ( { 'q' : searchQuery } )
 response = urllib.urlopen ( 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&' + query ).read()
 json = m_json.loads ( response )
 results = json [ 'responseData' ] [ 'results' ]
+
+
+
 for result in results:
-    title = result['title']
-    url = result['url']   # was URL in the original and that threw a name error exception
-    print ( title + '; ' + url )
+    title = result['titleNoFormatting']
+    #"""
+    if 'Business Insider' in title:
+        questionIndex = title.index('Business Insider')
+        newTitle = title[:questionIndex-3]
+        print newTitle
+    #"""
+    #print title
