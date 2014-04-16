88cd87700c433153aca349323f329e6a6db553af
arundhatijaswal <arundhatijaswal2013@u.northwestern.edu>
2014-04-16 15:15:54 -0500

    merge clean thesis generator

--- Test Code Thesis.py
+++ Test Code Thesis.py
@@ -1,6 +1,5 @@
 import urllib
 import json as m_json
-query = raw_input ( 'Query: ' )
 import requests, random, re, string
 from bs4 import BeautifulSoup
 
@@ -11,10 +10,6 @@ query = urllib.urlencode ( { 'q' : query } )
 response = urllib.urlopen ( 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&' + query ).read()
 json = m_json.loads ( response )
 results = json [ 'responseData' ] [ 'results' ]
-for result in results:
-    title = result['title']
-    url = result['url']   # was URL in the original and that threw a name error exception
-    print ( title + '; ' + url )
 
 thesisURL = results[random.randint(0, (len(results) - 1))]['url']
 r = requests.get(thesisURL)
