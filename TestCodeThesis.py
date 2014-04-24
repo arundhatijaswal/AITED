8004d380c95625f52b38a873b743d59aa6231d1c
Amanda <amanda.anumba@gmail.com>
2014-04-23 23:25:57 -0500

    test code amanda's working on

--- TestCodeThesis.py
+++ TestCodeThesis.py
@@ -1,20 +1,29 @@
 import urllib
+from pprint import pprint
 import json as m_json
 import requests, random, re, string, random
 from bs4 import BeautifulSoup
+from nltk.corpus import stopwords
 
 
 def genThesis():
+	""" google search API scraper that returns the 
+	title of an article based on the selected topic 
+	and keyword """
 	query = raw_input ( '\nTopic: ' )
-	# keyword = raw_input ( '\nkeyword: ' )
+	keyword = raw_input ( '\nkeyword: ' )
 	websites = {'businessinsider.com': 'h1', 'cnn.com': 'h1'}
 	web = random.choice(websites.keys())
-	queryText = 'reason why'
-	query = queryText + query + ' is important site:' + web
+	queryText = 'reason why '
+	query = queryText + query + ' and ' + keyword +' is important site:' + web
 	query = urllib.urlencode ( { 'q' : query } )
 	response = urllib.urlopen ( 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&' + query ).read()
 	json = m_json.loads ( response )
 	results = json [ 'responseData' ] [ 'results' ]
+	# print query 
+	# print json
+
+	print '\nSite searched: ', web
 	print 'length of results: ', len(results)
 
 	thesisURL = results[random.randint(0, (len(results) - 1))]['url']
@@ -22,21 +31,19 @@ def genThesis():
 	data = r.text
 	soup = BeautifulSoup(data)
 
-
-	#print thesisURL
 	if websites[web] == 'h1':
 	    thesisRaw = str(soup.h1)
 	else:
 	    print('other token')
 
-	#print thesisRaw
-	print '\nThesis: ', re.sub(r'<|>|\/|h1', r'', thesisRaw), '\n'
+	# print thesisRaw
+	topic = re.sub(r'<|>|\/|h1', r'', thesisRaw)
+	print '\nGenerated Topic: ', topic, '\n'
 
-	# to open the document and read the text
+	# send generated topic to stop words to be cleaned
+	# filtered_words = [w for w in topic.split() if not w in stopwords.words('english')]
+	# print filtered_words
 
-
-	#print thesis
-	# print(soup.get_text())
 
 def main():
 	genThesis()
---
+++ testcode - amanda/testcode.py
@@ -0,0 +1,182 @@
+import urllib
+from pprint import pprint
+import json as m_json
+import requests, random, re, string
+from bs4 import BeautifulSoup
+from nltk.corpus import stopwords
+from termcolor import colored
+# from __future__ import print_function
+# from alchemyapi import AlchemyAPI
+
+
+# Create the AlchemyAPI Object
+# alchemyapi = AlchemyAPI()
+# Global variables
+# APIkey = '7c97ba9b1d0135fec4970763a88200b94ea1e0c2'
+issues = []
+
+
+def genTopic():
+	""" google search API scraper that returns the 
+	title of an article based on the selected topic 
+	and keyword """
+	# topic = raw_input ( '\nEnter Topic: ' )
+	# keyword = raw_input ( 'Enter Keyword (optional): ' )
+	topic = 'education'
+	keyword = ''
+	# websites = {'www.usnews.com/opinion/articles': 'h1', 'cnn.com': 'h1', 'huffingtonpost.com': 'h1'}
+	# web = random.choice(websites.keys())
+	web = 'list25.com'
+	queryText = {'list of challenges within ': 'one', 'list of problems in ': 'two', 'top issues in ': 'three'}
+	querySearch = random.choice(queryText.keys())
+
+	if keyword == "":
+		query = querySearch + topic + ' site:' + web
+	else:
+		query = querySearch + topic + ' and ' + keyword + ' site:' + web
+
+	query = urllib.urlencode ( { 'q' : query } )
+	response = urllib.urlopen ( 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&' + query ).read()
+	json = m_json.loads ( response )
+
+	# stores the results of the google search
+	results = json [ 'responseData' ] [ 'results' ]
+	# print results.prettify()
+
+	if len(results) > 0:
+		URL = results[random.randint(0, (len(results) - 1))]['url']
+		r = requests.get(URL)
+		data = r.text
+		soup = BeautifulSoup(data)
+		# print soup.prettify()
+
+		# print URL 
+		print '\nURL: ', URL
+
+		# get the list items
+		print soup.findAll("div", {"class": "list-entry"})
+
+		
+	else:
+		print 'Search returned zero (0) results. Trying again...', '\n'
+		genTopic()
+
+
+
+
+
+# def genTopic():
+# 	""" google search API scraper that returns the 
+# 	title of an article based on the selected topic 
+# 	and keyword """
+# 	# topic = raw_input ( '\nEnter Topic: ' )
+# 	# keyword = raw_input ( 'Enter Keyword (optional): ' )
+# 	topic = 'education'
+# 	keyword = 'money'
+# 	# websites = {'www.usnews.com/opinion/articles': 'h1', 'cnn.com': 'h1', 'huffingtonpost.com': 'h1'}
+# 	# web = random.choice(websites.keys())
+# 	web = 'list25.com'
+# 	queryText = {'list of challenges within ': 'one', 'list of problems in ': 'two', 'top issues in ': 'three'}
+# 	querySearch = random.choice(queryText.keys())
+
+# 	if keyword == "":
+# 		query = querySearch + topic + ' site:' + web
+# 	else:
+# 		query = querySearch + topic + ' and ' + keyword + ' site:' + web
+
+# 	# query = queryText + keys + ' site:' + web
+# 	query = urllib.urlencode ( { 'q' : query } )
+# 	response = urllib.urlopen ( 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&' + query ).read()
+# 	json = m_json.loads ( response )
+
+# 	# stores the results of the google search
+# 	results = json [ 'responseData' ] [ 'results' ]
+# 	# print results.prettify()
+
+# 	if len(results) > 0:
+# 		# print '\nSite searched: ', web
+# 		# print 'length of results: ', len(results)
+
+# 		URL = results[random.randint(0, (len(results) - 1))]['url']
+# 		# URL = "money.cnn.com"
+# 		# URL = 'http://www.cnn.com/2011/12/02/opinion/vedder-college-costs/'
+# 		r = requests.get(URL)
+# 		data = r.text
+# 		soup = BeautifulSoup(data)
+# 		# print soup.prettify()
+
+# 		# title = str(soup.title).replace("<title>", "").replace("</title>", "")
+# 		# print title
+# 		print '\nURL: ', URL
+
+# 		response = alchemyapi.text('url', URL)
+
+# 		if response['status'] == 'OK':
+# 			# print('## Response Object ##')
+# 			# print(json.dumps(response, indent=4))
+
+# 			print('')
+# 			print('## Text ##')
+# 			print('text: ', response['text'].encode('utf-8'))
+# 			print('')
+# 		else:
+# 			print('Error in text extraction call: ', response['statusInfo'])	
+
+# 		# remove stopwords using nltk stop list
+# 		# keywords = [w for w in topic2.split() if not w in stopwords.words('english')]
+# 		# keys = ' '.join(keywords)
+# 		# print 'Stripped Keywords: ', keys, '\n'	
+
+# 		# if web == 'money.cnn.com': handleCNN(URL, soup)
+# 		# elif web == 'businessinsider.com': handleBI(URL, soup)
+# 		# elif web == 'huffingtonpost.com': handleHuff(URL, soup)
+# 		# r = requests.get("http://money.cnn.com/2013/10/24/news/economy/american-skills/")
+# 		# data = r.text
+# 		# soup = BeautifulSoup(data)
+# 		# handleCNN(URL, soup)
+# 	else:
+# 		print 'Search returned zero (0) results. Trying again...', '\n'
+# 		genTopic()
+
+
+
+# def handleCNN(link, data):
+# 	""" when searching on cnn.com, this is how to proceed """
+
+# 	# print title of article
+# 	title = str(data.title).replace("<title>", "").replace("</title>", "")
+# 	print '\nTopic: ', title, '\n'
+# 	print '====================================================', '\n'
+	
+# 	# check the url to see what site it went to, then handle accordingly
+# 	one = 'money.cnn.com'
+# 	two = 'schoolsofthought.blogs.cnn.com'
+# 	three = 'cnn.com'
+
+# 	if link[7:20] == one:
+# 		# print the subtitle of article as the topic
+# 		thesis = str(data.h2).replace("<h2>", "").replace("</h2>", "")
+# 		print 'Generated Thesis: ', colored(thesis, 'red'), '\n'
+# 		# if it's in <i> tags, get the first paragraph instead
+# 		# also, if it returns 'none' then try a different article
+# 	# else if link[7:37] == two:
+
+
+		
+	# print data.prettify()
+
+# print the first paragraph to see if it's a good thesis
+# content = soup.find("div", {"id": "storytext"}).p
+# print 'Generated Thesis: ', content
+
+# topic = "11 Reasons To Ignore The Haters And Major In The Humanities "
+# topic2 = "These Two Charts Prove A College Education Just Isn't Worth The Money Anymore"
+# topic3 = "Americans lacking in basic skills"
+# print '\nChosen Topic: ', topic2, '\n'
+
+
+def main():
+	genTopic()
+
+if __name__ == "__main__":
+	main()
